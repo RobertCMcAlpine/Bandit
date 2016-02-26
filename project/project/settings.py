0315c07dd212cb5a6a52f2617406004bdbb5ddc5
thkal <thod.kal@gmail.com>
2016-02-26 22:41:00 +0000

    Testing git: added a couple of directories we'll probably need, and updated settings.py & project/urls.py.

--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'  # the URL from which user-uploaded media is accessible
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # the local directory in which user-uploaded media is stored
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -36,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bandit',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +89,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+    )
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
