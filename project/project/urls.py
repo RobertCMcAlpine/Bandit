ab7ea232ebf2be23e86fad3f2fe4e467a5b061a2
Robert McAlpine <robert.mcalpine26@gmail.com>
2016-03-02 17:11:03 +0000

    Added database models to model.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # url(r'^blog/', include('blog.urls')), 
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^bandit/', include('bandit.urls')),
 )
