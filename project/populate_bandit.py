17d093450a1d7adea2dc544e3794537492d16ea0
thkal <thod.kal@gmail.com>
2016-03-16 20:56:15 +0000

    Population script finalised (tested + working)

--- project/populate_bandit.py
+++ project/populate_bandit.py
@@ -4,7 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 import django
 django.setup()
 
-from bandit.mdels import Profile, Band, Venue, Event, Request, Image
+from bandit.models import Profile, Band, Venue, Event, Request, Image
 
 from django.contrib.auth.models import User
 
@@ -13,37 +13,46 @@ from datetime import date, time
 def populate():
 	# Adding Leif's Band...
 	leif_user = add_user("leifos", "leifos@itech.com", "leifos")
-	leif_profile = add_profile(leif_user, "Leif's band", "Glasgow", "02392561857",
-		"The coolest band ever.", "B", "www.leifos.com")
+	leif_profile = add_profile(leif_user, "Leif's band", "-", "Glasgow",
+		"02392561857", "The coolest band ever.", "B", "www.leifos.com")
 	leif_band = add_band(leif_profile, 3, "Rock")
 
 	# Adding Crappy Maths LT Venue...
 	maths_user = add_user("david", "david@itech.com", "david")
-	maths_profile = add_profile(maths_user, "Crappy Maths Lecture Theatre", "Glasgow",
+	maths_profile = add_profile(maths_user, "Crappy Maths Lecture Theatre",  "-", "Glasgow",
 		"02392561857", "Built in 1950", "V", "www.mathsbld.com")
 	maths_venue = add_venue(maths_profile, "University Of Glasgow", "G12 8AB")
 
 	# Adding Laura's pub
 	laura_user = add_user("laura", "laura@itech.com", "david")
-	laura_profile = add_profile(laura_user, "Laura's Pub", "Glasgow",
+	laura_profile = add_profile(laura_user, "Laura's Pub",  "-", "Glasgow",
 		"02392561857", "Best pub in town.", "V", "www.lauraspub.co.uk")
 	laura_venue = add_venue(laura_profile, "7 Pub str", "G12 8AB")
 
-	# Adding an event...
+	# Adding an event for Crappy Maths LT
 	maths_event = add_event(maths_venue, leif_band, "ITECH Presentation", date(2016,3,23),
 		time(14,00,00), time(17,00,00), "An A1 in ITECH!", "The event of the year!")
 
-	# Adding another event...
+	# Adding an event for Laura's Pub
 	laura_event = add_event(laura_venue, "-", "Friday Night Live", date(2016,3,25),
 		time(19,30,00), time(22,00,00), "Free beer", "Beer and live music.")
 
+	# Adding a request from Leif's band to play in Laura's event
+	add_request(laura_event, leif_band)
 
+	# Adding a few pictures...
+	add_image(leif_profile, "media/images/leifos_prof.png", "profile pic")
+	add_image(laura_profile, "media/images/pub01.jpg", "pub pic")
+	add_image(laura_profile, "media/images/pub02.jpg", "pub pic")
+	add_image(laura_profile, "media/images/pub_prof.jpg", "prof pic")
+	add_image(maths_profile, "media/images/maths_prof.jpg", "prof pic")
 
 
 def add_user(username, email, password):
 	u = User.objects.get_or_create(username=username)[0]
 	u.email = email
 	u.password = password
+	u.save()
 	return u
 
 def add_profile(user, name, profile_picture, city, phone, desc, profile_type, website):
@@ -56,18 +65,20 @@ def add_profile(user, name, profile_picture, city, phone, desc, profile_type, we
 	p.description = desc
 	p.profile_type = profile_type
 	p.website = website
+	p.save()
 	return p
 
 def add_band(profile, number_of_members, genre):
-	b = Band.objects.get_or_create(profile=profile)[0]
-	b.number_of_members = number_of_members
+	b = Band.objects.get_or_create(profile=profile, number_of_members=number_of_members)[0]
 	b.genre = genre
+	b.save()
 	return b
 
 def add_venue(profile, address,post_code):
 	v = Venue.objects.get_or_create(profile=profile)[0]
 	v.address = address
 	v.post_code = post_code
+	v.save()
 	return v
 
 def add_event(venue, band, name, date, start_time, end_time, reward, description):
@@ -78,6 +89,7 @@ def add_event(venue, band, name, date, start_time, end_time, reward, description
 	e.end_time = end_time
 	e.reward = reward
 	e.description = description
+	e.save()
 	return e
 
 def add_request(event, band):
@@ -85,12 +97,12 @@ def add_request(event, band):
 	return r
 
 def add_image(profile, path, alt):
-	i=Image.objects.get_or_create(path=path)[0]
-	i.profile=profile
+	i=Image.objects.get_or_create(profile=profile,path=path)[0]
 	i.alt = alt
+	i.save()
 	return i
 
-if __name__ -- '__main__':
+if __name__ == '__main__':
 	print "Starting Bandit population script..."
 	populate()
 	print "... Done!"
