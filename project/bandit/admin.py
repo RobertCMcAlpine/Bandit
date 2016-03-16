2ab49fac84a3176a47295cc1a81358965148571b
thkal <thod.kal@gmail.com>
2016-03-16 20:31:52 +0000

    Registered all models to the admin page

--- project/bandit/admin.py
+++ project/bandit/admin.py
@@ -4,3 +4,8 @@ from bandit.models import Profile
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Band)
+admin.site.register(Venue)
+admin.site.register(Event)
+admin.site.register(Request)
+admin.site.register(Image)
