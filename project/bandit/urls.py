14d61fafb19caacd535fc289fb3e4ce036a03a51
thkal <thod.kal@gmail.com>
2016-03-19 00:06:17 +0000

    Added URL patterns for Band, Venue, Event, Request.

--- project/bandit/urls.py
+++ project/bandit/urls.py
@@ -8,4 +8,7 @@ urlpatterns = patterns('',
                url(r'^restricted/', views.restricted, name='restricted'),
                url(r'^logout/$', views.user_logout, name='logout'),
                url(r'^event/(?P<event_name_slug>[\w\-]+)/$', views.event, name='event'),
+               url(r'^band/(?P<profile_name_slug>[\w\-]+)/$', views.band, name='band'),
+               url(r'^venue/(?P<profile_name_slug>[\w\-]+)/$', views.venue, name='venue'),
+               url(r'^event/(?P<event_name_slug>[\w\-]+)/request/(?P<profile_name_slug>[\w\-]+)/$', views.request, name='request'),
                )
