42ffb45f1df3da02386f8d92eb0864cb76c509bc
thkal <thod.kal@gmail.com>
2016-03-19 00:07:59 +0000

    Imported missing module.

--- project/bandit/models.py
+++ project/bandit/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date,time
 from django.template.defaultfilters import slugify
+import os
 
 
 def get_image_path(instance, filename):
