fbaaa3122b486a51f253a2efb21e69feaa5f6db6
thkal <thod.kal@gmail.com>
2016-03-23 08:57:20 +0000

    Added test cases.

--- project/bandit/tests.py
+++ project/bandit/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
-# Create your tests here.
+from bandit.models import Band, Profile
+from django.contrib.auth.models import User
+
+
+class BandMethodTests(TestCase):
+	
+	def test_ensure_number_of_members_is_positive(self):
+		"""
+                ensure_number_of_members_is_positive should results True for bands where number of members is zero or positive
+        """
+                user = User(username='test', email='test@test.com', password='test')
+                user.save()
+                profile = Profile(name='test', user=user)
+                profile.save()
+                # This will succeed:
+                band = Band(profile=profile, number_of_members=3)
+                # This will fail:
+                # band = Band(profile=profile, number_of_members=-3)
+                band.save()
+                self.assertEqual((band.number_of_members >= 0), True)
+
+class ProfileMethodTests(TestCase):
+	
+	def test_slug_line_creation(self):
+            """
+            slug_line_creation checks to make sure that when we add a profile, an appropriate slug line is created
+            i.e. "Mouse Rat" -> "mouse-rat"
+             """
+
+            user = User(username='test', email='test@test.com', password='test')
+            user.save()
+            profile = Profile(name='Mouse Rat', user=user)
+            profile.save()
+            self.assertEqual(profile.slug, 'mouse-rat')
